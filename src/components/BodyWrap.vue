1ec7f18fd0f200c6d52633eddbfb6e4b7e25a58f
jaemin.yoon <>
2019-05-17 17:59:23 +0900

    초기 구성

---
+++ src/components/BodyWrap.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="bodyWrap">
+        bodyWrap
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Vue} from 'vue-property-decorator';
+
+    @Component
+    export default class BodyWrap extends Vue {
+
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+      <body-wrap></body-wrap>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import BodyWrap from '@/components/BodyWrap.vue'; // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld,
+    BodyWrap,
   },
 })
 export default class Home extends Vue {}
